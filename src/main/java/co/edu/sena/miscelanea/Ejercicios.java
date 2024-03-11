196f66dd22b6f769bc6f4844c5a034ceee7083f7
Luna <lunanino2705@gmail.com>
2024-03-10 20:00:07 -0500

    Se crea el menú y el menú de operadores

--- src/main/java/co/edu/sena/miscelanea/Ejercicios.java
+++ src/main/java/co/edu/sena/miscelanea/Ejercicios.java
@@ -1,3 +1,8 @@
 package co.edu.sena.miscelanea;
 
-
+public class Ejercicios {
+    
+    public double areaTriangulo(double dato1, double dato2){
+        return (dato1 * dato2) / 2;
+    }
+}
--- src/main/java/co/edu/sena/miscelanea/Main.java
+++ src/main/java/co/edu/sena/miscelanea/Main.java
@@ -10,7 +10,27 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
+        double dato1;
+         double dato2;
+         double dato3;
+         double dato4;
+
+
+        Ejercicios ejercicios = new Ejercicios();
+
+
+     
         
-         
+        //1.1
+        System.out.println("1.1 Calcular el área de un triángulo" );
+        Scanner src = new Scanner(System.in);
+
+
+        System.out.println("Ingrese la base del triángulo");
+        dato1 = src.nextDouble();
+        System.out.println("Ingrese la altura del triángulo");
+        dato2 = src.nextDouble();
+        dato3 = ejercicios.areaTriangulo(dato1, dato2);
+        System.out.println("El área del triánguo es: " + dato3);
 }
 }
