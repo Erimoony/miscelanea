f1013f9d250c1598d047c3411113eeb7f88ff6b5
Luna <lunanino2705@gmail.com>
2024-03-10 20:00:46 -0500

    Se crea el menú y el menú de operadores

--- src/main/java/co/edu/sena/miscelanea/Ejercicios.java
+++ src/main/java/co/edu/sena/miscelanea/Ejercicios.java
@@ -1,8 +1,3 @@
 package co.edu.sena.miscelanea;
 
-public class Ejercicios {
-    
-    public double areaTriangulo(double dato1, double dato2){
-        return (dato1 * dato2) / 2;
-    }
-}
+
--- src/main/java/co/edu/sena/miscelanea/Main.java
+++ src/main/java/co/edu/sena/miscelanea/Main.java
@@ -3,34 +3,94 @@ package co.edu.sena.miscelanea;
 
 import java.util.Scanner;
 
-
 /**
- * Este archivo es para el menu y submenu
+ * Menu y submenu
  *
  */
 public class Main {
     public static void main(String[] args) {
-        double dato1;
-         double dato2;
-         double dato3;
-         double dato4;
 
+        int option = 0;
 
-        Ejercicios ejercicios = new Ejercicios();
+        do {
+            System.out.println("-------- MENÚ DE LA MISCELANEA --------");
+            System.out.println("Escoge alguna de las siguientes opciones");
+            System.out.println("1. Operadores");
+            System.out.println("2. Condicionales");
+            System.out.println("3. Ciclos");
+            System.out.println("4. Arreglos");
+            System.out.println("99. Salir");
 
+            System.out.println("Selecciona una opción: ");
+            Scanner src = new Scanner(System.in);
+            option = src.nextInt();
 
-     
-        
-        //1.1
-        System.out.println("1.1 Calcular el área de un triángulo" );
+            switch (option) {
+                case 1:
+                    operadores();
+                    break;
+                case 2:
+                    Condicionales();
+                    break;
+                case 3:
+                    Ciclos();
+                    break;
+                case 4:
+                    Arreglos();
+                    break;
+                case 99:
+                    break;
+                default:
+                    System.out.println("Opción invalida");
+
+            }
+        } while (option != 99);
+        System.out.println("Has salido de la miscelanea");
+    }
+
+    static void operadores() {
+
+        System.out.println("-------- MENÚ DE OPERADORES --------");
+        System.out.println("Escoge alguna de los siguientes ejercicios");
+        System.out.println("1. Calcular el área de un triángulo");
+        System.out.println("2. Suma de dos números");
+        System.out.println("3. Número elevado al cuadrado");
+        System.out.println("4. Convertidor de euros a dólares");
+        System.out.println("5. Lado de un cuadrado mostrando el valor del área y del perímetro");
+        System.out.println("6. Área y volumen de un cilindro");
+        System.out.println("7. Radio de una circunferencia");
+        System.out.println("8. Promedio de 3 números");
+
+        System.out.println("Selecciona un ejercicio: ");
         Scanner src = new Scanner(System.in);
+        int opto = src.nextInt();
 
+        switch (opto) {
+            case 1:
+                area();
+                break;
+            case 2:
+                suma();
+                break;
+            case 3:
+                cuad();
+                break;
+            case 4:
+                conver();
+                break;
+            case 5:
+                lado();
+                break;
+            case 6:
+                cilin();
+                break;
+            case 7:
+                circu();
+                break;
+            case 8:
+                prom();
+                break;
 
-        System.out.println("Ingrese la base del triángulo");
-        dato1 = src.nextDouble();
-        System.out.println("Ingrese la altura del triángulo");
-        dato2 = src.nextDouble();
-        dato3 = ejercicios.areaTriangulo(dato1, dato2);
-        System.out.println("El área del triánguo es: " + dato3);
-}
+        }
+    }
 }
