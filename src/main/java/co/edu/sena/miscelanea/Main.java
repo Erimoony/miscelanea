1bcb95eb56645377b07dd9147f73f09594684987
Luna <lunanino2705@gmail.com>
2024-03-10 19:48:56 -0500

    se crea el repositorio

---
+++ src/main/java/co/edu/sena/miscelanea/Main.java
@@ -0,0 +1,16 @@
+package co.edu.sena.miscelanea;
+
+
+import java.util.Scanner;
+
+
+/**
+ * Este archivo es para el menu y submenu
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+         
+}
+}
